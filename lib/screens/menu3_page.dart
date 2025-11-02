1d51338b8ce952762eb1f842933e4f7e1ed2f966
Mubarok982 <rizqymubarok99@gmail.com>
2025-11-02 19:39:08 +0700

    update UI

--- lib/screens/menu3_page.dart
+++ lib/screens/menu3_page.dart
@@ -1,12 +1,78 @@
 import 'package:flutter/material.dart';
 
-class Menu3Page extends StatelessWidget {
+class Menu3Page extends StatefulWidget {
   const Menu3Page({super.key});
 
   @override
+  State<Menu3Page> createState() => _Menu3PageState();
+}
+
+class _Menu3PageState extends State<Menu3Page> {
+  int jumlah = 0;
+
+  void tambahAngka() {
+    setState(() {
+      jumlah++;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Menu 3 (Masih Kosong)', style: TextStyle(fontSize: 22)),
+    return Scaffold(
+      backgroundColor: const Color(0xFFFAF4FF), // warna lembut
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Jumlah: $jumlah',
+              style: const TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                color: Colors.black87,
+              ),
+            ),
+            const SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: tambahAngka,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                foregroundColor: Colors.deepPurple,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                elevation: 2,
+              ),
+              child: const Text(
+                'Tambah Angka',
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+            const SizedBox(height: 15),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.grey,
+                foregroundColor: Colors.deepPurple,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                elevation: 3,
+              ),
+              child: const Text(
+                'Kembali',
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
